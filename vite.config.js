95ef8068450534ffd5f2ca7ee59b5c7ab81d4318
Kristina Ra4kovi4 <103757850+Ra4kovi4@users.noreply.github.com>
2023-08-05 14:37:37 +0300

    add 404.html fix index.html

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), reactRefresh()],
+	base: "/eliftech_delivery-service-frontend/",
 	build: {
-		outDir: "dist",
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					"404.html": ["./404.html"],
+				},
+			},
+		},
 	},
 });
