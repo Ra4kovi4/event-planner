683c70e7ebbfee7b12995dff190200f9f73ee7c0
Kristina Ra4kovi4 <103757850+Ra4kovi4@users.noreply.github.com>
2023-09-19 23:47:30 +0300

    upd basename

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
-import { lazy } from "react";
-import { Routes, Route } from "react-router-dom";
-import { Layout } from "./components/Layout/Layout";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import "./App.css";
+import { lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Layout } from './components/Layout/Layout';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './App.css';
 
-const MainPage = lazy(() => import("./Pages/MainPage"));
-const EventPage = lazy(() => import("./Pages/EventPage"));
-const NewEventPage = lazy(() => import("./Pages/NewEvent"));
-const EditEventPage = lazy(() => import("./Pages/EditEventPage"));
+const MainPage = lazy(() => import('./Pages/MainPage'));
+const EventPage = lazy(() => import('./Pages/EventPage'));
+const NewEventPage = lazy(() => import('./Pages/NewEvent'));
+const EditEventPage = lazy(() => import('./Pages/EditEventPage'));
 
 const App = () => {
-	return (
-		<>
-			<Routes>
-				<Route path='/' element={<Layout />}>
-					<Route index element={<MainPage />} />
-					<Route path='/:id' element={<EventPage />} />
-					<Route path='/add' element={<NewEventPage />} />
-					<Route path='/:id/edit' element={<EditEventPage />} />
-					<Route path='*' element={<MainPage />} />
-				</Route>
-			</Routes>
-			<ToastContainer autoClose={1000} />
-		</>
-	);
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path="/:id" element={<EventPage />} />
+          <Route path="/add" element={<NewEventPage />} />
+          <Route path="/:id/edit" element={<EditEventPage />} />
+          <Route path="*" element={<MainPage />} />
+        </Route>
+      </Routes>
+      <ToastContainer autoClose={1000} />
+    </>
+  );
 };
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react(), reactRefresh()],
-  base: '/',
+  base: '/events',
   build: {
     rollupOptions: {
       output: {
