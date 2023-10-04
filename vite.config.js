dede7cf0153e469e743e441da4997bf8e423de74
Kristina Ra4kovi4 <103757850+Ra4kovi4@users.noreply.github.com>
2023-10-04 15:52:24 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,19 +5,9 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [svgr(), react()],
-    base: '/events/',
+
     esbuild: {
         jsxFactory: 'h',
         jsxFragment: 'Fragment',
-    },
-    output: {
-        publicPath: '/',
-    },
-    build: {
-        rollupOptions: {
-            output: {
-                dir: 'dist/events',
-            },
-        },
     },
 });
